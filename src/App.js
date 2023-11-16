2f5a765581fe7fdc1f47237b6267b97be232aa80
Nathan Towe <nathanwtowe89@gmail.com>
2023-11-15 21:06:32 -0500

    finished demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  // const [name, setName] = useState('Nathan')
+
+  // setTimeout(() => {
+  //   setName('Dustin')
+  // }, 3500)
+
+  const [counter, setCounter] = useState(0)
+
+  const increment = () => {
+    setCounter(currantValue => {
+      return currantValue + 1
+    })
+  }
+
+  const decrement = () => {
+    if(counter === 0) return
+    setCounter(currantValue => {
+      return currantValue - 1
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3> counter: {counter}</h3>
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
 
 export default App;
+
+// Don't manipulate the DOM directly
+//don't ever ever ever change a stateful variable directly
